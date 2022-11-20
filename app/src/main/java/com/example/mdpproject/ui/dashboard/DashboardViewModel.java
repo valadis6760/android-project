922abceb070085522ad5064985fca81496d7c35d
Valantis <valadis6760@gmail.com>
2022-11-20 13:59:26 +0100

    dashboard vm clean

--- app/src/main/java/com/example/mdpproject/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/mdpproject/ui/dashboard/DashboardViewModel.java
@@ -1,17 +1,11 @@
 package com.example.mdpproject.ui.dashboard;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class DashboardViewModel extends ViewModel {
 
-
    public int user_height;
 
     public DashboardViewModel() {
-
     }
-
-
 }
